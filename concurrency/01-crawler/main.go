73d6989c6de1bcf66589e270e516e9b692558eb5
schaffe <artur.dzidzoiev@gmail.com>
2026-06-26 14:21:40 -0700

    bucket

--- concurrency/01-crawler/main.go
+++ concurrency/01-crawler/main.go
@@ -1,25 +1,82 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"math"
 	"sync"
 	"time"
 )
 
-var limiter <-chan time.Time
+var limiter *TokenBucket
+
+type TokenBucket struct {
+	mu         sync.Mutex
+	tpsRate    float64
+	tpsTokens  float64
+	lastRefill time.Time
+}
+
+func NewTokenBucket(tpsRate float64) *TokenBucket {
+	t := TokenBucket{
+		tpsTokens:  tpsRate,
+		tpsRate:    tpsRate,
+		lastRefill: time.Now(),
+	}
+
+	return &t
+}
+
+// must be called inside critical section
+func (t *TokenBucket) refill() {
+	curr := time.Now()
+	timeDiff := curr.Sub(t.lastRefill).Seconds()
+
+	tokens := t.tpsRate * timeDiff
+	//fmt.Printf("refill %f\n", tokens)
+
+	t.tpsTokens = min(t.tpsRate, t.tpsTokens+tokens)
+	t.lastRefill = curr
+}
+
+func (t *TokenBucket) Wait(ctx context.Context) {
+	for {
+		t.mu.Lock()
+		t.refill()
+
+		if t.tpsTokens >= 1.0 {
+			t.tpsTokens--
+			t.mu.Unlock()
+			return
+		}
+
+		missingTokens := 1 - t.tpsTokens
+		duration := time.Duration(math.Ceil(missingTokens / t.tpsRate * float64(time.Second)))
+
+		//println(t.tpsTokens)
+		//println(duration)
+
+		t.mu.Unlock()
+
+		select {
+		case <-time.After(duration):
+		case <-ctx.Done():
+			println("Context cancelled")
+			return
+		}
+	}
+}
 
 type Fetcher interface {
 	Fetch(url string) (string, []string, error)
 }
 
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(ctx context.Context, url string, depth int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	<-limiter
+	limiter.Wait(ctx)
 
-	f := fetcher
-
-	l, links, err := f.Fetch(url)
+	l, links, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -33,18 +90,21 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 
 	wg.Add(len(links))
 	for _, l := range links {
-		go Crawl(l, depth-1, wg)
+		go Crawl(ctx, l, depth-1, wg)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	limiter = time.Tick(time.Second)
+	limiter = NewTokenBucket(1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	wg.Add(1)
 
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl(ctx, "http://golang.org/", 4, &wg)
 
 	wg.Wait()
 }
